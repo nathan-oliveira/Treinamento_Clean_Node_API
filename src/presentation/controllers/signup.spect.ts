216f20d2d1b1f17667eeff08a37611a5fb72938b
Nathan Oliveira <52627147+nathan-oliveira@users.noreply.github.com>
2020-08-15 18:53:41 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.spect.ts
+++ src/presentation/controllers/signup.spect.ts
@@ -29,4 +29,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'nathan',
+        email: 'nathan@email.com',
+        passwordConfirmation: '123456'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+  
 })
